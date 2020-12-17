7fb0e53ff60cab8f258c4ddf86ca0014bf2fdc6d
benjamin <benjamin.tropper@gmail.com>
2020-12-18 01:42:00 +0200

    adding react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import BookList from './BookList.js';
+import AddBook from './AddBook.js';
+import UpdateBook from './UpdateBook.js';
+import DeleteBook from './DeleteBook.js';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div width={'100%'}style={{backgroundColor: "blue"}}>
+      <h1 style={{color: "black"}}>OAMK Library</h1>
+      </div>
+      <DeleteBook/>
+      <AddBook/>
+      <UpdateBook/>
+      <div>
+      <BookList/>
+      </div>
     </div>
   );
 }
